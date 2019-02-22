9087f20bc05ab8ab0400ab94402f2885ad8ca6b6
gauravk0105 <gauravvk0105@gmail.com>
2019-02-22 18:04:50 +0530

    LL1 Parser up

--- Lab2/lab2_2.cpp
+++ Lab2/lab2_2.cpp
@@ -15,7 +15,7 @@ map<string, vector<string> >::iterator it;
 vector<string> v, visited;
 set<string> T;
 set<string> NT; 
-map<string, vector<string> > intialGrammar, resultGrammar;
+map<string, vector<string> > intial, result;
 set<string> s, vis;
 map< string ,map<string, vector<string> > > table;
 
@@ -38,15 +38,16 @@ void openFile()
                  p+=second[i];
                  NT.insert(p);
             }
-		intialGrammar[first].push_back(second);
+		intial[first].push_back(second);
 	}
+	result = intial;
 }
 
 
-void findLR()
+void getLR()
 {
 	string str;
-	for(it = intialGrammar.begin(); it != intialGrammar.end(); ++it){
+	for(it = intial.begin(); it != intial.end(); ++it){
 		str = it->first;
 		v = it->second;
 		int ind;
@@ -63,39 +64,39 @@ void findLR()
 
 string buildNewVariable()
 {
-	static int count = -1;
-	count++;
+	static int parse = -1;
+	parse++;
 	char x = 'L';
-	for(int i = 0; i < count; ++i) x += 1;
+	for(int i = 0; i < parse; ++i) x += 1;
     string ret(1, x); 
     return ret;
 }
 
-void removeLR()
+void ridOfLR()
 {
 	for(int i = 0; i < visited.size(); ++i){
 		string add = buildNewVariable();	
 		string str = visited[i];
-		v = resultGrammar[str];
-		resultGrammar.erase(str);
+		v = result[str];
+		result.erase(str);
 		int ind;
 		for(int j = 0; j < v.size(); ++j){
 			string y = "";
             y+=v[j][0];
 			if(y.compare(str)!=0){									//For Non Recursive elements in recursive Non Terminal
-				resultGrammar[str].push_back(v[j] + add);
+				result[str].push_back(v[j] + add);
 			}
 			else{													//For Recursive elements in recursive Non Terminal
-				resultGrammar[add].push_back(v[j].substr(str.length()) + add);
+				result[add].push_back(v[j].substr(str.length()) + add);
 			}
 		}
-		resultGrammar[add].push_back("#");
+		result[add].push_back("#");
 	}
 }
 
 void leftFactoring()
 {
-	for(it = resultGrammar.begin(); it != resultGrammar.end(); ++it){
+	for(it = result.begin(); it != result.end(); ++it){
         vector<string> pre;
 		v = it->second;
 		sort(v.begin(), v.end());
@@ -133,52 +134,29 @@ void leftFactoring()
                         v[j] = "@";
                     }
                 }
-                resultGrammar[add] = v2;
+                result[add] = v2;
             }
             for(int i = 0; i < v.size(); ++i) if(v[i] != "@") v1.push_back(v[i]);
-            resultGrammar[it->first] = v1;
+            result[it->first] = v1;
         }
-	}
-}
-
-void printSetMap(map<string, set<string> > sm)
-{
-	for(map<string, set<string> >::iterator i = sm.begin(); i != sm.end(); ++i)
-	{
-		cout<<i->first<<" : { ";
-
-		s = i->second;
-		for(set<string>::iterator j = s.begin(); j != s.end(); ++j)
-		{
-			if(j != s.begin()) cout<<", ";
-			cout<<*j;
-		}
-		cout<<" }"<<endl;
 	}
 }
 
 set<string> findFirst(string k)
 {
-	//cout<<"Visiting For Key -> "<<k<<endl;
 	if(vis.find(k) != vis.end()) return First[k];
 	vis.insert(k);
-	v = resultGrammar[k];
-
-	for(int i = 0; i < v.size(); ++i)
-	{
+	v = result[k];
+	for(int i = 0; i < v.size(); ++i){
 		string str = v[i];
-		for(int j = 0; j < str.length(); ++j)
-		{
+		for(int j = 0; j < str.length(); ++j){
 			if(str[j] >= 65 && str[j] <= 90)						//Reading a Non Terminal
 			{
-				//cout<<"Recursing For "<<str[j]<<endl;
 				s = findFirst(string(1, str[j]));
-
 				First[k].insert(s.begin(), s.end());
 
 				if(s.find("#") == s.end()) break;
-				else if(j != str.length() - 1)
-                {
+				else if(j != str.length() - 1){
                     First[k].erase(First[k].find("#"));
                 }
 			}
@@ -195,43 +173,33 @@ set<string> findFirst(string k)
 
 set<string> findFollow(string k)
 {
-    //cout<<"Visiting For Key -> "<<k<<endl;
     if(vis.find(k) != vis.end()) return Follow[k];
 	vis.insert(k);
-
-	for(map<string, vector<string> >::iterator temp = resultGrammar.begin(); temp != resultGrammar.end(); ++temp)
-	{
+	for(map<string, vector<string> >::iterator temp = result.begin(); temp != result.end(); ++temp){
 		string nt = temp->first;
 		v = temp->second;
 		for(int i = 0; i < v.size(); ++i)
 		{
-            size_t ind = v[i].find(k);
+            int ind = v[i].find(k);
             if(ind == string::npos) continue;
-            for(ind = ind+1; ind < v[i].length(); ++ind)
-            {
-                if(v[i][ind] >= 65 && v[i][ind] <= 90)
-                {
-                    //cout<<"Storing First of "<<v[i][ind]<<" in "<<k<<endl;
+            for(ind = ind+1; ind < v[i].length(); ++ind){
+                if(v[i][ind] >= 65 && v[i][ind] <= 90){
                     Follow[k].insert(First[string(1, v[i][ind])].begin(), First[string(1, v[i][ind])].end());
                     Follow[k].erase(Follow[k].find("#"));
-                    if(First[string(1, v[i][ind])].find("#") == First[string(1, v[i][ind])].end()) break;
+                    if(First[string(1, v[i][ind])].find("#") == First[string(1, v[i][ind])].end()) 
+						break;
                 }
-                else
-                {
-                    //cout<<"Storing "<<v[i][ind]<<" in "<<k<<endl;
+                else{
                     Follow[k].insert(string(1, v[i][ind]));
                     break;
                 }
             }
-            if(ind == v[i].length())
-            {
-                //cout<<"Recursing For "<<nt<<endl;
+            if(ind == v[i].length()){
                 s = findFollow(nt);
                 Follow[k].insert(s.begin(), s.end());
             }
 		}
 	}
-
 	return Follow[k];
 }
 
@@ -252,14 +220,11 @@ void showCurrentGrammar(map<string, vector<string> > grammar)
 
 void createLLParserTable()
 {
-    for(map<string, vector<string> >::iterator it = resultGrammar.begin(); it != resultGrammar.end(); ++it)
-    {
+    for(map<string, vector<string> >::iterator it = result.begin(); it != result.end(); ++it){
         string nt = it->first;
 		v = it->second;
-        for(int i=0;i<v.size();i++)
-        {
-            if(v[i][0]>='a' && v[i][0]<='z')
-            {
+        for(int i=0;i<v.size();i++){
+            if(v[i][0]>='a' && v[i][0]<='z'){
                 string res = nt + " -> " + v[i];
                 map<string,vector<string> > m;
                 m = table[nt];
@@ -270,31 +235,26 @@ void createLLParserTable()
                 m[p] = f;
                 table[nt] = m;
             }
-            else
-            {
+            else{
                 string p = "";
                 p+=v[i][0];
-                if(p.compare("#")==0)
-                {    
+                if(p.compare("#")==0){    
                     string res = nt + " -> " + v[i];
                     set<string> g = Follow[nt];
                     map<string,vector<string> > m = table[nt];
-                    for(set<string>::iterator j = g.begin(); j != g.end(); ++j)
-		            {
+                    for(set<string>::iterator j = g.begin(); j != g.end(); ++j){
 			            vector<string> f = m[*j];
                         f.push_back(res);
                         m[*j] = f;
 		            }
                     table[nt] = m;
                 }
-                else
-                {
+                else{
                     string str = v[i];
                     string res = nt + " -> " + v[i];
                     set<string> s;
                     map<string,vector<string> > m = table[nt];
-		            for(int j = 0; j < str.length(); ++j)
-		            {
+		            for(int j = 0; j < str.length(); ++j){
 			            if(str[j] >= 65 && str[j] <= 90)						//Reading a Non Terminal
 			            {
 				            //cout<<"Recursing For "<<str[j]<<endl;
@@ -324,37 +284,18 @@ void createLLParserTable()
             }
         }
     }
-    
     cout << "\n";
     cout << "___________________LL(1) Parsing TABLE__________________\n\n";
-    cout << "NT/T \t" << " ";
-    // for(set<string>::iterator j = NT.begin(); j != NT.end(); ++j)
-	// {
-	// 	cout << ""  <<  *j << "       ";
-	// }
     cout << "\n\n";
-    for(set<string>::iterator j = T.begin(); j != T.end(); ++j)
-	{
+    for(set<string>::iterator j = T.begin(); j != T.end(); ++j){
 		cout << *j << "  ::   ";
         map<string,vector<string> > m = table[*j];
         map<string,vector<string> >::iterator it =m.begin();
-        for(it=m.begin();it!=m.end();it++)
-        {
-        //    //int count=0;
-        //     {
-        //             cout << it->second << "," << it->first << "       "; break;
-        //          }
+        for(it=m.begin();it!=m.end();it++){
         cout << " ( " << it->first << " , ";
-        
         vector<string> f = it->second;
         for(int i=0;i<f.size();i++)
             cout << f[i] << " )      ";
-            //  for(set<string>::iterator j = NT.begin(); j != NT.end(); ++j)
-		    //  {
-            //      {
-            //         cout << it->second << "," << it->first << "       "; break;
-            //      }
-		    //  }  
         }
         cout << "\n\n";
 	}
@@ -362,9 +303,9 @@ void createLLParserTable()
 
 void buildRRGrammar()
 {
-    cout<<"The actual grammar with or without Left Recursion is : "<<endl;
+    cout<<"Given Grammar :: "<<endl;
     string str;
-	for(it =    intialGrammar.begin(); it != intialGrammar.end(); ++it){
+	for(it =    intial.begin(); it != intial.end(); ++it){
 		str = it->first;
 		cout<<str<<" -> ";
 		v = it->second;
@@ -376,10 +317,10 @@ void buildRRGrammar()
 	}
     cout<<"_____________________________________________________________________"<<endl<<endl;
 	cout<<"_____________________________________________________________________"<<endl<<endl;
-    findLR();
-	removeLR();
-    cout<<"The changed grammar withour left recursion is : "<<endl<<endl;
-	showCurrentGrammar(resultGrammar);
+    getLR();
+	ridOfLR();
+    cout<<"Grammar After Removing Left Recursion :: "<<endl<<endl;
+	showCurrentGrammar(result);
     cout<<"_____________________________________________________________________"<<endl<<endl;
 	cout<<"_____________________________________________________________________"<<endl<<endl;
 }
@@ -387,20 +328,27 @@ void buildRRGrammar()
 void doDeterministicGrammar()
 {
     leftFactoring();
-    cout<<"The Deterministic grammar is : "<<endl;
-	showCurrentGrammar(resultGrammar);
+    cout<<"Building the Deterministic Grammar :: "<<endl;
+	showCurrentGrammar(result);
     cout<<"_____________________________________________________________________"<<endl<<endl;
 	cout<<"_____________________________________________________________________"<<endl<<endl;
 }
 
 void calculateFirst()
 {
-    for(it = resultGrammar.begin(); it != resultGrammar.end(); ++it)
-	{
+    for(it = result.begin(); it != result.end(); ++it){
 		findFirst(it->first);
 	}
-	cout<<"First is : "<<endl;
-	printSetMap(First);
+	cout<<"Calcuated First of Each Variable :: "<<endl;
+	for(map<string, set<string> >::iterator i = First.begin(); i != First.end(); ++i){
+		cout<<i->first<<" : { ";
+		s = i->second;
+		for(set<string>::iterator j = s.begin(); j != s.end(); ++j){
+			if(j != s.begin()) cout<<", ";
+			cout<<*j;
+		}
+		cout<<" }"<<endl;
+	}
     cout<<"_____________________________________________________________________"<<endl<<endl;
 	cout<<"_____________________________________________________________________"<<endl<<endl;
 }
@@ -410,13 +358,20 @@ void calculateFollow()
     vis.clear();
 	Follow["S"].insert("$");
     findFollow("S");
-	for(it = resultGrammar.begin(); it != resultGrammar.end(); ++it)
-	{
+	for(it = result.begin(); it != result.end(); ++it){
 		findFollow(it->first);
 	}
-
-	cout<<"Follow is : "<<endl;
-	printSetMap(Follow);
+	cout<<"Calculated Follow of Each Variable :: "<<endl;
+	for(map<string, set<string> >::iterator i = Follow.begin(); i != Follow.end(); ++i){
+		cout<<i->first<<" : { ";
+		s = i->second;
+		for(set<string>::iterator j = s.begin(); j != s.end(); ++j)
+		{
+			if(j != s.begin()) cout<<", ";
+			cout<<*j;
+		}
+		cout<<" }"<<endl;
+	}
     cout<<"_____________________________________________________________________"<<endl<<endl;
 	cout<<"_____________________________________________________________________"<<endl<<endl;
 }
@@ -424,12 +379,11 @@ void calculateFollow()
 int main()
 {
     openFile();
-    resultGrammar = intialGrammar;
     buildRRGrammar();
     doDeterministicGrammar();
     calculateFirst();
     calculateFollow();
-    for(it = resultGrammar.begin(); it != resultGrammar.end(); ++it){
+    for(it = result.begin(); it != result.end(); ++it){
 		string str = it->first;
         T.insert(str);
 		v = it->second;
@@ -437,11 +391,10 @@ int main()
 			string p = "";
             p+=v[i];
             for(int i=0;i<p.length();i++)
-            if(p[i]>='a' && p[i]<='z')
-            {
+            if(p[i]>='a' && p[i]<='z'){
                 string x ="";
                 x+=p[i];
-                 NT.insert(x);
+                NT.insert(x);
             }
 		}
 	}
